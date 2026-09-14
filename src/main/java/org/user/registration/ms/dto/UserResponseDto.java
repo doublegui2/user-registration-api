cb6dad593945ee03ca8e193dc4326e10c8e375ea
gui2 <guillaume1.gac@gmail.com>
2026-09-14 17:20:20 +0200

    [evol]: created Controller for user registration + exception handler

---
+++ src/main/java/org/user/registration/ms/dto/UserResponseDto.java
@@ -0,0 +1,38 @@
+package org.user.registration.ms.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import org.user.registration.ms.exception.IllegalBirthdateFormatException;
+import org.user.registration.ms.exception.IllegalCountryException;
+import org.user.registration.ms.validator.ISO8601Validator;
+
+import java.time.Instant;
+import java.util.Locale;
+
+public record UserResponseDto(
+        @NotBlank
+        String username,
+
+        @NotBlank
+        String birthdate,
+
+        @NotBlank
+        String countryOfResidence,
+
+        String phoneNumber,
+
+        String gender,
+
+        @NotBlank
+        Instant createdAt
+) {
+    public UserResponseDto {
+        // Check if the birthdate follows the appropriate ISO-8601 format
+        if (!ISO8601Validator.isValidISO8601(birthdate)) {
+            throw new IllegalBirthdateFormatException("Birthdate must follow the ISO-8601 format: 'yyyy-mm-dd'");
+        }
+        // Check if the country corresponds to a valid country code
+        if (!Locale.getISOCountries(Locale.IsoCountryCode.PART1_ALPHA3).contains(countryOfResidence)) {
+            throw new IllegalCountryException("Country of residence must be a valid 3-letter code (ex: 'USA', 'FRA'");
+        }
+    }
+}
